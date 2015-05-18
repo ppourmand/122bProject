2e6d33e2c8339fc91fb87988bb2b2991edb0071f
Pasha Pourmand <pasha.pourmand@gmail.com>
2015-05-18 16:26:31 -0700

    added some polynomials and parameters

--- project.py
+++ project.py
@@ -1,15 +1,20 @@
 #!/usr/bin/env python3
-#Anirudh Agarwala
-#Ojas Goyal
-#Pasha Pourmand
-#Miguel Sanchez
+# Anirudh Agarwala
+# Ojas Goyal
+# Pasha Pourmand
+# Miguel Sanchez
 
-def naive():
+# multiplies two polynomials together
+# polynomials may have differing degrees 
+def naive(poly1, poly2):
+
 
 def fft():
 
 def main():
-
+  polynomial_one = [4,3,-2,10]
+  polynomial_two = [4,3,-2,10]
+  naive(polynomial_one,polynomial_two)
 
 if __name__ == '__main__':
   main()
