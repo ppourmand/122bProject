5999c60938ae2b3ea4cbc9ea5d6b33bc6037bfda
Pasha Pourmand <pasha.pourmand@gmail.com>
2015-05-18 17:36:59 -0700

    made variable names more clear

--- project.py
+++ project.py
@@ -10,13 +10,11 @@ def naive(poly1, poly2):
   degrees. It does so naively, ie "foil" method
   """
 
-  # initialize result to how many coefficients will be the result
   result = [0]*(len(poly1)+len(poly2)-1)
 
-  # builds the coefficients
-  for o1,i1 in enumerate(poly1):
-    for o2,i2 in enumerate(poly2):
-        result[o1+o2] += i1*i2
+  for power1,coeff1 in enumerate(poly1):
+    for power2,coeff2 in enumerate(poly2):
+        result[power1+power2] += coeff1*coeff2
 
   return result
 
