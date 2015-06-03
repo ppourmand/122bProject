1b59513a41805f3a77f330b568c1e663485c5ba9
Miguel Sanchez <Miguel@campus-057-251.ucdavis.edu>
2015-06-02 18:24:16 -0700

    Tested for 1 case, need to test for more cases. Should be done after testing

--- FFT.py
+++ FFT.py
@@ -2,11 +2,18 @@ __author__ = 'Miguel'
 import numpy
 from cmath import exp, pi
 
+def padExtra(x):
+
+    n = len(x)
+    for i in range(0, n):
+        x.append(0)
+    return x
+
 
 def fft(x):
     # x is a coefficient vector, from lowest to highest power
     n = len(x)
-    d = [0] * n
+    d = [0] * (n)
     if n <= 1:
         return x
     even = fft(x[0::2])  # we get every 2 powers, starting at 0(evens)
@@ -17,27 +24,21 @@ def fft(x):
         d[k] = even[k] + odd[k] * w
         d[k + n / 2] = even[k] - odd[k] * w
         w = wn * w
-    return d
+    return numpy.array(d)
 
-fftA = fft([-10, 1, -1, 7])
-fftB = fft([3, -6, 0, 8])
 
-c = []
+def polyMult(a,b):
+    # Function to multiply polynomials using fft
+    fftA = fft(padExtra(a))  # manually padded right now. will have to create a pad function
+    fftB = fft(padExtra(b))
 
-for a, b in zip(fftA, fftB):
-    c.append(a * b)
+    solution = numpy.fft.ifft(numpy.multiply(fftB, fftA)).real
+    solution = [int(round(i)) for i in solution]
+    finalSolution = [solution[0]]
+    finalSolution.extend(solution[len(solution):1:-1])
+    return finalSolution
 
-C = numpy.array(c)
-solution = numpy.fft.ifft(C)
-print c
-test = []
-tester = numpy.array([-10, 1, -1, 7])
-tester = numpy.fft.fft(tester, 2 * len(tester) - 1)
-tester2 = numpy.array([-3, -6, 0, 8])
-tester2 = numpy.fft.fft(tester2, 2 * len(tester) - 1)
-for a,b in zip(tester,tester2):
-    test.append(a * b)
-print numpy.fft.ifft(test)
+print polyMult([-10, 1, -1, 7], [3, -6, 0, 8])
 
 
 
